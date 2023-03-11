f1470b332717d53c6aebf4addc7403ba45caa99b
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2023-03-12 02:37:20 +0300

    the end

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,14 +1,20 @@
-﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+﻿/*Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше 
+либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения 
+алгоритма. При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
+*/
+
+
+string[] array1 = new string[5]{ "234", "gb", "hello", "world", "ceo" };
 string[] array2 = new string[array1.Length];
 void SecondArray(string[] array1, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-    if(array1[i].Length <= 3)
+        if (array1[i].Length <= 3)
         {
-        array2[count] = array1[i];
-        count++;
+            array2[count] = array1[i];
+            count++;
         }
     }
 }
@@ -16,10 +22,13 @@ void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} ");
+
+        Console.Write($" {array[i]} ");
     }
     Console.WriteLine();
 }
+Console.WriteLine("Заданный массив 234, gb, hello, world, ceo");
+Console.Write("Результат выборки: ");
 SecondArray(array1, array2);
 PrintArray(array2);
 
